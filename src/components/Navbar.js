dd8b4dd88069013d7b95dcff42b47bd618f9ae22
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-08-25 10:50:51 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar=()=>{
+return (
+    <nav>
+        <img src="../images/logo.svg" alt="logo"/>
+        <ul>
+            <li><a href="#!">Features</a></li>
+            <li><a href="#!">Pricing</a></li>
+            <li><a href="#!">Resources</a></li>
+        </ul>
+        <ul>
+            <li><a href="#!">Login</a></li>
+            <li><a href="#!">Sign up</a></li>
+        </ul>
+    </nav>
+)
+};
+export default Navbar;
