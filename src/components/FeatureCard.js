19079a4ea61637498ee4a26875ad0f359e9bc819
Debadutta Panda <debadebaduttapanda.7@gmail.com>
2022-08-26 15:44:49 +0530

    build feature card

--- src/components/FeatureCard.js
+++ src/components/FeatureCard.js
@@ -1,4 +1,10 @@
-const FeatureCard=()=>{
-
+const FeatureCard=(props)=>{
+    return <div className={props.className}>
+        <div className={`${props.className}__img-container`}>
+        <img src={props.icon} alt={props.title}/>
+        </div>
+        <h2>{props.title}</h2>
+        <p>{props.description}</p>
+    </div>;
 };
 export default FeatureCard;
